e87789fe01936b9b52eb5193259aa1363a69290c
Vinícius Ares <vinicius.ares12@gmail.com>
2026-09-04 11:09:13 -0300

    finalização lista 1.2.3

---
+++ src/main/java/lista/lista1_3_3/rentcars/Cliente.java
@@ -0,0 +1,67 @@
+package lista.lista1_3_3.rentcars;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Cliente {
+    private String nome;
+    private List<Locacao> carrosAlugados = new ArrayList<Locacao>();
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void adicionaLocacao(Locacao locacao) {
+        carrosAlugados.add(locacao);
+    }
+
+    public String extrato() {
+        final String fimDeLinha = System.getProperty("line.separator");
+        int sequencia = 0;
+
+        Iterator<Locacao> locacoes = carrosAlugados.iterator();
+        String resultado = "Registro de Alugueis de " + getNome() + fimDeLinha;
+        resultado += String.format("Seq Automovel               Ano da Locacao Valor Pago" + fimDeLinha);
+        resultado += String.format("=== ==================== ============== ===========" + fimDeLinha);
+
+        while (locacoes.hasNext()) {
+            Locacao locacao = locacoes.next();
+
+            // mostra valores para este aluguel
+            sequencia++;
+            resultado += String.format("%02d. %-20s %4d R$ %8.2f" + fimDeLinha,
+                    sequencia, locacao.getCarro().getDescricao(),
+                    locacao.getCarro().getAno(), locacao.valorLocacao());
+        } // while
+
+        // adiciona rodapé
+        resultado += "============================================" + fimDeLinha;
+        resultado += String.format("Valor Acumulado em diárias............: R$ %8.2f" + fimDeLinha, getValorTotal());
+        resultado += "Voce acumulou " + getPontosAlugadorFrequente() +
+                " pontos de alugador frequente";
+        return resultado;
+    }
+
+    public int getPontosAlugadorFrequente() {
+        int pontosDeAlugadorFrequente = 0;
+        Iterator<Locacao> locacoes = carrosAlugados.iterator();
+        while (locacoes.hasNext()) {
+            pontosDeAlugadorFrequente += locacoes.next().pontosLocacao();
+        }
+        return pontosDeAlugadorFrequente;
+    }
+
+    public double getValorTotal() {
+        double valorTotal = 0.0;
+        Iterator<Locacao> locacoes = carrosAlugados.iterator();
+        while (locacoes.hasNext()) {
+            valorTotal += locacoes.next().valorLocacao();
+        }
+        return valorTotal;
+    }
+}
